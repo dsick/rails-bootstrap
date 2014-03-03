09a6ec36d746af2557dc427e1bde7bc6a0edae1f
Dave Sick <sick@davesick.net>
2014-03-03 10:07:22 -0800

    add modal to home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,24 @@
-class VisitorsController < ApplicationController
+lass VisitorsController < ApplicationController
 
   def new
-    flash[:notice] = 'Welcome!'
-    flash[:alert] = 'Access not allowed.'
- 
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.update_spreadsheet
+      flash[:notice] = "Chose #{@visitor.favorite}."
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:favorite, :comment)
   end
 
 end
